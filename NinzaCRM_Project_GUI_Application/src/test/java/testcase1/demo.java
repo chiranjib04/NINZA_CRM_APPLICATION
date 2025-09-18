e842f0a3eb85851c9911681c3a5634ab1487dee1
chiranjib04 <51132352+chiranjib04@users.noreply.github.com>
2025-09-18 18:21:48 +0530

    chaange

--- NinzaCRM_Project_GUI_Application/src/test/java/testcase1/demo.java
+++ NinzaCRM_Project_GUI_Application/src/test/java/testcase1/demo.java
@@ -1,9 +1,10 @@
 package testcase1;
 
+import org.junit.Ignore;
 import org.testng.annotations.Test;
 
 public class demo {
-
+@Ignore
 	@Test
 	public void demoTest() {
 		System.out.println("hello");
